a55a567322eb82ab9444c268b29ad96bb2ad669f
Mario Ortiz Manero <marioortizmanero@gmail.com>
2019-10-24 00:59:01 +0200

    Platform choosing, config

---
+++ src/config.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <QApplication>
+#include <QSettings>
+#include "config.h"
+
+
+template <typename T>
+struct option {
+    std::string section;
+    T defaultValue;
+};
+
+
+void parseConfig() {
+    QSettings settings("spotivids", "spotivids");
+    settings.setPath(QSettings::IniFormat, QSettings::UserScope, "spotivids.conf");
+    std::cout << settings.fileName().toStdString() << std::endl;
+
+    settings.beginGroup("/Defaults");
+    std::cout << settings.value("fullscreen").toBool() << std::endl;
+    std::cout << settings.value("test").toInt() << std::endl;
+    settings.endGroup();
+    exit(0);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include <chrono>
+#include "config.h"
 #include "player/vlc.h"
-#include "api/linux.h"
 
+#if defined(__linux__) || defined(__sun) || defined(__FreeBSD) || defined(__NetBSD__) || defined(__OpenBSD__)
+#    define USE_LINUX_API true
+#    include "api/linux.h"
+#elif defined(_WIN32)
+#    define USE_WINDOWS_API true 
+#elif defined(__APPLE__)
+#    define USE_MACOS_API true
+#else
+#    define USE_WEB_API true
+#endif
 
 void printUsage();
 void printVersion();
@@ -12,6 +22,7 @@ int main(int argc, char *argv[]) {
     // Argument variables
     bool lyrics = true;
     bool fullscreen = false;
+    bool useWebApi = false;
 
     // Argument parsing
     if (argc >= 2) {
@@ -35,21 +46,39 @@ int main(int argc, char *argv[]) {
             else if (arg == "--fullscreen" || arg == "-f") {
                 fullscreen = true;
             }
+
+            else if (arg == "--use-web-api" || arg == "-w") {
+                useWebApi = true;
+            }
         }
     }
+
+    parseConfig();
     
     // Hides stderr if debug is disabled
 #ifndef DEBUG
     freopen("/dev/null", "w", stderr);
 #endif
 
-    // Chooses the used API depending on the platform
-#ifdef __linux__
+    // Choosing the used API depending on the platform
+    // The Web API is the default when the system isn't supported, and can be
+    // enabled with a flag within the program.
+#ifndef USE_WEB_API
+    if (useWebApi) {
+#endif
+        std::cout << "Web API" << std::endl;
+#ifndef USE_WEB_API
+    }
+#endif
+
+#ifdef USE_LINUX_API
     VLCPlayer player;
     DBusAPI<VLCPlayer> spotify(player);
     playVideosDBus<VLCPlayer>(player, spotify, lyrics);
-#else
-    std::cout << "spotivids only works on linux for now.\n";
+#elif defined(USE_WINDOWS_API)
+    std::cout << "Windows" << std::endl;
+#elif defined(USE_MACOS_API)
+    std::cout << "macOS" << std::endl;
 #endif
 
     return 0;
